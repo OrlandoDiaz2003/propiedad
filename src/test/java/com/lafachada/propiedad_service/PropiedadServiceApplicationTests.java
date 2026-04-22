309e461d3925a3855ea1dc7eca29accb6a86de46
Orlando <169166413+OrlandoDiaz2003@users.noreply.github.com>
2026-04-21 21:35:17 -0400

    first commit

---
+++ src/test/java/com/lafachada/propiedad_service/PropiedadServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lafachada.propiedad_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropiedadServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
